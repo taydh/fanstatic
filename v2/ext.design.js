9641c5f521d92d6d32917d0f9b2694580773cf69
Taufan <2606621+taydh@users.noreply.github.com>
2025-10-19 11:19:25 +0700

    update ext design
    unit, axis, and wrapper now inside fanstatic.design

--- v2/ext.design.js
+++ v2/ext.design.js
@@ -1,25 +1,35 @@
 {
 	Object.assign(fanstatic, {
-		unit: function(model, attributes = {}) {
-			let tagFill = ('div ' + Object.entries(attributes).map(([k,v]) => `${k}="${v}"`).join()) + ' data-design="unit"';
-			let result = {};
+		design: {
+			wrapper: function(model, attributes = {}) {
+				let tagFill = ('div ' + Object.entries(attributes).map(([k,v]) => `${k}="${v}"`).join()) + ' data-design="wrapper"';
+				let result = {};
 
-			result[tagFill] = [];
+				result[tagFill] = Array.isArray(model) ? model : model.items;
 
-			if (model.head) result[tagFill].push({'div data-design="unit-head"': model.head});
-			if (model.body) result[tagFill].push({'div data-design="unit-body"': model.body});
-			if (model.foot) result[tagFill].push({'div data-design="unit-foot"': model.foot});
+				return result;
+			},
+			unit: function(model, attributes = {}) {
+				let tagFill = ('div ' + Object.entries(attributes).map(([k,v]) => `${k}="${v}"`).join()) + ' data-design="unit"';
+				let result = {};
 
-			return result;
-		},
-		axis: function(model, attributes = {}) {
-			let tagFill = ('div ' + Object.entries(attributes).map(([k,v]) => `${k}="${v}"`).join()) + ' data-design="axis"';
-			let result = {};
+				result[tagFill] = [];
 
-			result[tagFill] = model.items;
+				if (model.head) result[tagFill].push({'div data-design="unit-head"': model.head});
+				if (model.body) result[tagFill].push({'div data-design="unit-body"': model.body});
+				if (model.foot) result[tagFill].push({'div data-design="unit-foot"': model.foot});
 
-			return result;
-		},
+				return result;
+			},
+			axis: function(model, attributes = {}) {
+				let tagFill = ('div ' + Object.entries(attributes).map(([k,v]) => `${k}="${v}"`).join()) + ' data-design="axis"';
+				let result = {};
+
+				result[tagFill] = Array.isArray(model) ? model : model.items;
+
+				return result;
+			},
+		}
 	});
 	
 	window.dispatchEvent(new CustomEvent('fanstatic.design.load', {
