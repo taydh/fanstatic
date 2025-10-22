47b60d45a85cffa3c39861eef75c6539a686f600
Taufan <2606621+taydh@users.noreply.github.com>
2025-10-22 21:27:37 +0700

    Update themes

--- v2/themes/bogor-bootstrap/theme.js
+++
@@ -1,3 +0,0 @@
-fanstatic.insertStyles([
-	fanstatic.settings.base_url + fanstatic.settings.version + '/themes/bogor-bootstrap/theme.css?' + fanstatic.tail()
-]);
---
+++ v2/themes/bootstrap5/theme.js
@@ -0,0 +1,3 @@
+fanstatic.insertStyles([
+	fanstatic.settings.base_url + fanstatic.settings.version + `/themes/bootstrap5/${fanstatic.settings.theme_edition}/theme.css?` + fanstatic.tail()
+]);
